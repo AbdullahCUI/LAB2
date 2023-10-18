79d382d5bc3af9b13485a8594b93320cba42453e
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-18 21:22:06 +0500

    declared a variable

--- area.c
+++ area.c
@@ -12,19 +12,5 @@
 
 int main(int argc, char **argv) {
 
-  double area, base, height;
-
-  printf("Please enter the base of a triangle: ");
-
-  scanf("%lf", &base);
-
-  printf("Please enter the height of a triangle: ");
-
-  scanf("%lf", &height);
-
-  area = 1.0/2*  base * height ;
-
-  printf("The area is %f square units.\n", area);
-
-  return 0;
+ double area,base,height;
 }
