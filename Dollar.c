5904a16b0e3b7ae0ed3155290f7b2d00ed557e5b
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-18 22:23:07 +0500

    declared other variables and and predefine function

--- Dollar.c
+++ Dollar.c
@@ -10,5 +10,17 @@ int main()
     const float pound=0.79f;
 
     float exchange_rate=0.10f;
+
+    float USD;
+
+    float JPN_convert, Pound_convert,remaing_usd;
+
+    //promote value of USD
+
+    printf("Please Enter USD:");
+
+    //to read USD from user
+
+    scanf("%f", &USD);
     return 0;
 }
